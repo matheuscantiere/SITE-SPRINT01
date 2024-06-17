52579f3d3bd950f20ab97fa7e5d4cb75e6c024c9
Guilherme Barbiero <133436901+GuiBarbiero@users.noreply.github.com>
2024-06-17 18:23:11 -0300

    arrumando login

--- login.js
+++ login.js
@@ -1,4 +1,3 @@
-
 const layout = document.getElementById('layout');
 const registreBtn = document.getElementById('registre');
 const loginBtn = document.getElementById('login');
@@ -11,31 +10,31 @@ loginBtn.addEventListener('click', () => {
     layout.classList.remove("active");
 });
 
-// login com if e else
+function entrar(event) {
+    event.preventDefault();  // Prevent the default form submission
 
-function entrar(){
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+    var username = document.getElementById("login-email").value;
+    var password = document.getElementById("login-password").value;
 
-    if (username == "matheus" && password == "oi") {
+    if (username === "matheus@tamole.com" && password === "oi") {
         alert("Logado com sucesso!");
-        window.open('index2.html');
-    }else {
+        window.open('Pagina01.html');
+    } else {
         alert("Usuário ou senha incorretos");
     }
-    
 }
 
-function cadastrar() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+function cadastrar(event) {
+    event.preventDefault();  // Prevent the default form submission
 
+    var username = document.getElementById("register-username").value;
+    var email = document.getElementById("register-email").value;
+    var password = document.getElementById("register-password").value;
 
-  if (username.trim() == "" || password.trim() == "") {
-      alert("Por favor, preencha todos os campos.");
-      return;
-  }else{
-    alert("Conta criada com sucesso!");
-    window.open('pagina2.html', '_blank');
-  }  
+    if (username.trim() === "" || email.trim() === "" || password.trim() === "") {
+        alert("Por favor, preencha todos os campos.");
+    } else {
+        alert("Conta criada com sucesso!");
+        window.open('pagina2.html', '_blank');
+    }  
 }
